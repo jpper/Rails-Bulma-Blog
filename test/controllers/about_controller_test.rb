c190b52f0d8b2066544e86214f3a0d2a6ad65168
Jonathan Perry <jpper@umich.edu>
2019-08-10 12:41:01 +0900

    Correctly added the about route and view

--- test/controllers/about_controller_test.rb
+++ test/controllers/about_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class AboutControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get show" do
+    get about_show_url
+    assert_response :success
+  end
+
 end
